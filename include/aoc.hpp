7572d72310289751fc732c1922b2ad19f0e8d74a
Philipp Krüger <github.khab3@passinbox.com>
2025-12-16 21:36:45 +0100

    Add 'split' helper function for string splitting

--- include/aoc.hpp
+++ include/aoc.hpp
@@ -4,7 +4,23 @@
 #include <cassert>
 #include <charconv>
 #include <cmath>
+#include <ranges>
 #include <string_view>
+#include <vector>
+
+constexpr auto view2string = [](auto&& view) {
+    return std::string{std::begin(view), std::end(view)};
+};
+
+constexpr auto split_view(const std::string& string, char delim = '\n') {
+    return string | std::views::split(delim) | std::views::transform(view2string);
+}
+
+constexpr auto split_transformer(char delim) {
+    return std::views::transform([delim](const std::string& string) {
+        return split_view(string, delim);
+    });
+}
 
 template <typename T>
 auto str2int(std::string_view string) -> T {
@@ -12,6 +28,11 @@ auto str2int(std::string_view string) -> T {
     const auto [ptr, ec] = std::from_chars(std::begin(string), std::end(string), number);
     assert(ec == std::errc());
     return number;
+}
+
+inline std::vector<std::string> split(const std::string& string, char delim = '\n') {
+    auto view{split_view(string, delim)};
+    return std::vector<std::string>{std::begin(view), std::end(view)};
 }
 
 template <typename T>
